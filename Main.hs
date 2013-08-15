8193fbbdd681b867f34953765648ca1fe27578db
Johan Liesén <johan@liesen.se>
2013-08-16 00:43:46 +0200

    Remove old code

--- Main.hs
+++ Main.hs
@@ -14,11 +14,35 @@ import Text.Pandoc hiding (Writer)
 import Text.ParserCombinators.Parsec (runParser)
 import qualified Language.CalDims.State as S
 
-evalCodeBlock :: Block -> Block
-evalCodeBlock (CodeBlock attrs s) = CodeBlock attrs (concat . intersperse "\n" . execWriter . evalCodeBlock' S.start $ s)
-  where
-    evalCodeBlock' state = foldM evalCodeLine state . lines
-evalCodeBlock block = block
+readDoc :: String -> Pandoc
+readDoc = readMarkdown def
+
+writeDoc :: Pandoc -> String
+writeDoc = writeMarkdown def
+
+type HeaderLevel = Int
+
+instance Monoid S.State where
+  mempty = S.start
+  state1 `mappend` state2 = let scope = getScope state1 `mappend` getScope state2
+                                args = getArgs state1 `mappend` getArgs state2
+                                argValues = getArgValues state1 `mappend` getArgValues state2
+                            in S.State scope args argValues
+
+transformDoc :: Pandoc -> Pandoc
+transformDoc = flip evalState (0, [S.start]) . everywhereM (mkM evalCodeBlock)
+
+evalCodeBlock :: MonadState (HeaderLevel, [S.State]) m => Block -> m Block
+evalCodeBlock (CodeBlock attrs s) = do (depth, state:states) <- get
+                                       let (state', codelines') = runWriter . foldM evalCodeLine state . lines $ s
+                                           s' = concat . intersperse "\n" $ codelines'
+                                       put (depth, state':states)
+                                       return (CodeBlock attrs s')
+evalCodeBlock header@(Header k _ _) = do (depth, state:states) <- get
+                                         when (k < depth) $ put (k, drop (depth - k) states)
+                                         when (k > depth) $ put (k, replicate (k - depth) state ++ states)
+                                         return header
+evalCodeBlock block = return block
 
 evalCodeLine state line = let (state', line') = case runParser parseLine state "" line of
                                                   Left err -> (state, line)
@@ -31,37 +55,4 @@ evalCodeLine state line = let (state', line') = case runParser parseLine state "
                           in do tell [line']
                                 return state'
 
-readDoc :: String -> Pandoc
-readDoc = readMarkdown def
-
-writeDoc :: Pandoc -> String
-writeDoc = writeMarkdown def
-
-transformDoc :: Pandoc -> Pandoc
-transformDoc = bottomUp evalCodeBlock
-
-type HeaderLevel = Int
-
-instance Monoid S.State where
-  mempty = S.start
-  state1 `mappend` state2 = let scope = getScope state1 `mappend` getScope state2
-                                args = getArgs state1 `mappend` getArgs state2
-                                argValues = getArgValues state1 `mappend` getArgValues state2
-                            in S.State scope args argValues
-
-transformDocM :: Pandoc -> Pandoc
-transformDocM = flip evalState (0, [S.start]) . everywhereM (mkM evalCodeBlockM)
-
-evalCodeBlockM :: MonadState (HeaderLevel, [S.State]) m => Block -> m Block
-evalCodeBlockM (CodeBlock attrs s) = do (depth, state:states) <- get
-                                        let (state', codelines') = runWriter . foldM evalCodeLine state . lines $ s
-                                            s' = concat . intersperse "\n" $ codelines'
-                                        put (depth, state':states)
-                                        return (CodeBlock attrs s')
-evalCodeBlockM header@(Header k _ _) = do (depth, state:states) <- get
-                                          when (k < depth) $ put (k, drop (depth - k) states)
-                                          when (k > depth) $ put (k, replicate (k - depth) state ++ states)
-                                          return header
-evalCodeBlockM block = return block
-
-main = interact (writeDoc . transformDocM . readDoc)
+main = interact (writeDoc . transformDoc . readDoc)
